ccb2a2638ed8c21e0f53d5a1266161ce3489919b
Kingfreak <noahzonneveld@hotmail.com>
2021-10-05 16:48:54 +0200

    update

--- duizelig/week.py
+++ duizelig/week.py
@@ -1,33 +1,25 @@
-w = 1
-d = input('dag: ')
-if d == 'ma':
-    d = 2
-elif d == 'di':
-    d = 3
-elif d == 'wo':
-    d = 4
-elif d == 'do':
-    d = 5
-elif d == 'vr':
-    d = 6
-elif d == 'za':
-    d = 7
-elif d == 'zo':
-    d = 8
+dag = 'start'
+day = input('What day is it?: ')
 
-while w != d:
-    if w == 1:
-        print('maandag')
-    elif w == 2:
-        print('dinsdag')
-    elif w == 3:
-        print('woensdag')
-    elif w == 4:
-        print('donderdag')
-    elif w == 5:
-        print('vrijdag')
-    elif w == 6:
-        print('zaterdag')
-    elif w == 7:
-        print('zondag')
-    w = w + 1
+while dag != day:
+    if dag == 'start':
+        print('ma')
+        dag = 'ma'
+    elif dag == 'ma':
+        print('di')
+        dag = 'di'
+    elif dag == 'di':
+        print('wo')
+        dag = 'wo'
+    elif dag == 'wo':
+        print('do')
+        dag = 'do'
+    elif dag == 'do':
+        print('vr')
+        dag = 'vr'
+    elif dag == 'vr':
+        print('za')
+        dag = 'za'
+    elif dag == 'za':
+        print('zo')
+        dag = 'zo'
